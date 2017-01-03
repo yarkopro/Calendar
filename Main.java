637a3d280e543ecf173f8378edb4511f7161efea
yarkopro <yarkopro@gmail.com>
2017-01-03 04:18:32 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -11,6 +11,7 @@ public class Main {
             System.err.println("Incorrect month value.");
         } catch (Exception e){
             System.err.println("Some error occurred.");
+            e.printStackTrace();
         }
     }
 }
